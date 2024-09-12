c4a0cdd87cf561d6c03693dc52bedcb18ba09c7e
Lenny Gonzalez <badil17@yahoo.com>
2024-09-12 13:36:50 -0400

    modelo rutas y controladores para citas

--- backend/routes/citasRutas.js
+++ backend/routes/citasRutas.js
@@ -1,8 +1,25 @@
 const express = require("express");
+require("../middleware/authMiddleware");
 const router = express.Router();
 const citaController = require("../controllers/citas.controlador");
 
-router.get("/citas", citaController.getCita);
+// GET: Listar citas
+router.get("/citas", citaController.citas);
+
+// POST: Crear nueva cita
+router.post("/crear-cita", citaController.crearCita);
+
+// GET: Obtener detalles de una cita
+router.get("/citas/:id", citaController.UnicaCita);
+
+// PUT: Actualizar información de una cita
+router.put("/citas/:id", citaController.actualizarCita);
+
+// DELETE: Cancelar una cita
+router.delete("/citas/:id", citaController.eliminarCita);
+
+// POST: Enviar recordatorio de cita
+router.post("/citas/:id/notificacion", citaController.eliminarCita);
 
 // router.use(authMiddleware);
 
