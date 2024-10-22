fe8601bc5087a7a24993f20a9b9ea51f285ba048
Lenny Gonzalez <98600094+Morefew@users.noreply.github.com>
2024-10-22 12:47:15 -0400

    refactor: refactorizar  el modelo de las citas El modelo citas ahora tiene los nombres similares a la BD del cliente

--- backend/models/cita.model.js
+++ backend/models/cita.model.js
@@ -2,14 +2,9 @@ const mongoose = require("mongoose");
 
 const citaMedicaSchema = new mongoose.Schema(
   {
-    paciente_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Paciente",
-      required: [true, "Por favor seleccione el paciente para la cita"],
-    },
-    doctor_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: [true, "Por favor seleccione el Doctor para la cita"],
+    especialista: {
+      type: String,
+      required: [true, "Por favor indique el nombre del Doctor para la cita"],
     },
     fecha: {
       type: Date,
@@ -19,9 +14,12 @@ const citaMedicaSchema = new mongoose.Schema(
       type: String,
       required: [true, "Por favor indique una hora para la cita"],
     },
-    motivo: {
+    tipo_servicio: {
       type: String,
-      required: [true, "Por favor indique el motivo de la cita"],
+      required: [
+        true,
+        "Por favor indique el tipo de servicio a solicitar de la cita",
+      ],
       trim: true,
     },
     estado: {
@@ -29,9 +27,62 @@ const citaMedicaSchema = new mongoose.Schema(
       enum: ["confirmada", "pendiente"],
       default: "pendiente",
     },
-    notificaciones: {
-      type: Boolean,
-      default: false,
+    paciente_nombre: {
+      type: String,
+      trim: true,
+      required: [true, "Por favor seleccione el paciente para la cita"],
+    },
+    paciente_edad: {
+      type: Number,
+      required: [true, "Por favor indique la edad del paciente"],
+    },
+    paciente_nacionalidad: {
+      type: String,
+      trim: true,
+      required: [true, "Por favor indique la nacionalidad del paciente"],
+    },
+    paciente_cedula: {
+      type: String,
+      trim: true,
+      required: [true, "Por favor indique la cedula del paciente"],
+    },
+    paciente_genero: {
+      type: String,
+      trim: true,
+      required: [true, "Por favor indique el genero del paciente"],
+    },
+    paciente_direccion: {
+      type: String,
+      trim: true,
+      required: [true, "Por favor indique la dirección del paciente"],
+    },
+    tipo_paciente: {
+      type: String,
+      trim: true,
+    },
+    solicitante_nombre: {
+      type: String,
+      trim: true,
+      required: [
+        true,
+        "Por favor indique el nombre de quien solicita el servicio al paciente",
+      ],
+    },
+    solicitante_apellido: {
+      type: String,
+      trim: true,
+      required: [
+        true,
+        "Por favor indique el apellido de quien solicita el servicio al paciente",
+      ],
+    },
+    afiliacion_ars: {
+      type: String,
+      trim: true,
+    },
+    centro_nombre: {
+      type: Number,
+      required: [true, "Por favor indique el nombre del centro para la cita"],
     },
   },
   {
